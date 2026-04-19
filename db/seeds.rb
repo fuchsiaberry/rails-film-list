6cb780197da3893d6584e55f7371e4a876df2f18
steven roekens <stevenroekens@gmail.com>
2026-04-19 13:08:30 +0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "json"
+require "open-uri"
+
+puts "Cleaning database ..."
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+puts "Database cleaned!"
+
+puts "Fetching Data"
+url = "https://tmdb.lewagon.com/movie/top_rated"
+response = URI.open(url).read
+data = JSON.parse(response)
+
+data["results"].each do |movie|
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
+    rating: movie["vote_average"],
+  )
+end
+
+puts "Seeding complete, #{Movie.count} Movies created"
